16c34af325a37235165023225dedf9cddcff53fb
Evgeniy Chernyy <e.chernyj@notamedia.ru>
2024-11-26 22:54:58 +0300

    ResidentRequestPopup - added form submit

--- src/components/ResidentRequestPopup.vue
+++ src/components/ResidentRequestPopup.vue
@@ -19,45 +19,60 @@ const placeTypeOptions = [{
 document.addEventListener("OpenResidentRequestPopup",()=>{
   opened.value = true
 })
+
+  function submitResidentRequestForm(){
+    fetch("api/V1/resident.php",{
+      method: 'POST',
+      body: new FormData(document.forms.residentRequestForm)
+    })
+    .then((response)=>{
+      if(response.ok) return response.json()
+      else throw new Error("Что-то пошло не так")
+    })
+    .then((result) => alert(result.text))
+    .catch((error) => alert(error))
+  }
 </script>
 
 <template>
   <div class="popup" :class="{active:opened}">
-    <div class="popup__header">
-      <p class="popup__title">Заполните заявку, чтобы стать резидентом</p>
-    </div>
-    <div class="popup__content">
-      <TextInput
-        :label="'Наименование организации / ИП'"
-        :name="'companyName'"
-      />
-      <TextInput
-        :label="'Контактный телефон'"
-        :name="'contactPhone'"
-        :mask="'+7 (###) ###-##-##'"
-      />
-      <BaseSelect
-        :label="'Тип помещения'"
-        :inputName="'placeType'"
-        :options="placeTypeOptions"
-      />
-      <TextInput
-        :label="'Адрес'"
-        :name="'address'"
-      />
-      <BaseRange
-        :label="'Площадь помещения (м²)'"
-        :inputName="'square'"
-        :type="'number'"
-      />
-      <BaseRange
-        :label="'Дата начала аренды'"
-        :inputName="'rentDate'"
-        :type="'date'"
-      />
-    </div>
-    <div class="popup__footer">
-      <button class="btn">Отправить</button>
-    </div>
+    <form id="residentRequestForm">
+      <div class="popup__header">
+        <p class="popup__title">Заполните заявку, чтобы стать резидентом</p>
+      </div>
+      <div class="popup__content">
+        <TextInput
+          :label="'Наименование организации / ИП'"
+          :inputName="'companyName'"
+        />
+        <TextInput
+          :label="'Контактный телефон'"
+          :inputName="'contactPhone'"
+          :mask="'+7 (###) ###-##-##'"
+        />
+        <BaseSelect
+          :label="'Тип помещения'"
+          :inputName="'placeType'"
+          :options="placeTypeOptions"
+        />
+        <TextInput
+          :label="'Адрес'"
+          :inputName="'address'"
+        />
+        <BaseRange
+          :label="'Площадь помещения (м²)'"
+          :inputName="'square'"
+          :type="'number'"
+        />
+        <BaseRange
+          :label="'Дата начала аренды'"
+          :inputName="'rentDate'"
+          :type="'date'"
+        />
+      </div>
+      <div class="popup__footer">
+        <button class="btn" @click="submitResidentRequestForm" type="button">Отправить</button>
+      </div>
+    </form>
   </div>
 </template>
